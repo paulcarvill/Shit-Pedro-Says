845877aff7ffc3274a93440974ca16ec87089417
Paul Carvill <paul@paulcarvill.com>
2013-08-20 11:21:15 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShitPedroSays::Application.config.secret_key_base = '2aac77f7d8be545b0de14a5bf271ee0818df1e78551afb12803bcee99aa5eedd3d9eac0af1cc71064cb4d2e70f520d28c18bc9828d09b6537e24453a6fa79669'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ShitPedroSays::Application.config.session_store :cookie_store, key: '_shit-pedro-says_session'
