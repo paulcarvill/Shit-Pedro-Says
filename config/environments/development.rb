431cb6488c4427c913ac8e618d92fe2d4c5f72fa
Paul Carvill <paul@paulcarvill.com>
2013-09-07 10:10:35 +0100

    cleaning out all the ember stuff

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,4 @@ ShitPedroSays::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-
-  config.ember.variant = :development
 end
