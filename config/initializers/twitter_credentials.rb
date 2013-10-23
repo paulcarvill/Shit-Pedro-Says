1b37e6f90b94599c59c43460d59228346b8aa8fc
Dom Geargeoura <Dom.Geargeoura@rga.com>
2013-10-23 13:26:18 +0100

    added twitter creds

---
+++ config/initializers/twitter_credentials.rb
@@ -0,0 +1,9 @@
+require 'twitter'
+
+Twitter.configure do |config|
+  config.consumer_key = 'qO7jb5zhlLfXFx6XC3Zmw'
+  config.consumer_secret = 'Rn4cha27hTUnTiewlLcCGJUXukfJnX4i4JxBKYzApLo'
+  config.oauth_token = '1676069250-sxNJiaCMbnjg96j91MZV7EKxmlIIJIVJUCHlewi'
+  config.oauth_token_secret = 'G01xPtOb2q7fuU5OMOZE0dOggW5t1klxMZXjrxH2lrkJY'
+end
+
