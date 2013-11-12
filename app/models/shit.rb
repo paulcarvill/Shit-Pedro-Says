79578e2b6e4d02b039cd19bcba3118eebd06a43e
Paul Carvill <paul@paulcarvill.com>
2013-11-12 21:41:44 +0000

    limiting slug to first five words of Shit t keep the URLs nice and small

--- app/models/shit.rb
+++ app/models/shit.rb
@@ -1,6 +1,14 @@
 class Shit < ActiveRecord::Base
 	extend FriendlyId
-  	friendly_id :content, use: :slugged
+  	friendly_id :first_five_words, use: :slugged
+
+  	def first_five_words
+  		content.split(/\s+/)[0..4].compact.join(" ")
+	end
+
+	def should_generate_new_friendly_id?
+    	true
+  	end
 
 	def next
     	self.class.first(:conditions => ["id > ?", id], :order => "id asc")
